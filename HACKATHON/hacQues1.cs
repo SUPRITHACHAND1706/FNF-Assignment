23fe946940253acae66d7300472626b76baf38e6
SUPRITHACHAND1706 <160388279+SUPRITHACHAND1706@users.noreply.github.com>
2025-08-08 11:23:42 +0530

    Add files via upload

---
+++ HACKATHON/hacQues1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon
+{
+    internal class hacQues1
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the input:");
+            string input = Console.ReadLine();
+            freqCounter(input);
+        }
+        static void freqCounter(string input)
+        {
+            if (!input.Any(char.IsLetter))
+            {
+                Console.WriteLine("0");
+                return;
+            }
+
+            input = input.ToLower();
+
+            char[] CharClean = input.Select(c => char.IsLetter(c) || c == ' ' ? c : ' ').ToArray();
+            string cleanInput = new string(CharClean);
+
+            string[] words = cleanInput.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> freq = new Dictionary<string, int>();
+            foreach (string word in words)
+            {
+                if (freq.ContainsKey(word))
+                {
+                    freq[word]++;
+                }
+                else
+                {
+                    freq[word] = 1;
+                }
+            }
+
+            var sortedWords = freq.OrderByDescending(x => x.Value).ThenByDescending(x => x.Key);
+
+            foreach (var pair in sortedWords)
+            {
+                Console.WriteLine($"{pair.Value} {pair.Key}");
+            }
+            
+        }
+        
+    }
+}
+
+
---
+++ HACKATHON/hacQues2.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon
+{
+    internal class Book
+    {
+        static (string Title, string FirstAuthor) ParseEntry(string entry)
+
+        {
+
+            int titleStart = entry.IndexOf('\"') + 1;
+
+            int titleEnd = entry.IndexOf('\"', titleStart);
+
+            string title = entry.Substring(titleStart, titleEnd - titleStart);
+
+            int authorStart = entry.IndexOf(" by ") + 4;
+
+            string authorPart = entry.Substring(authorStart);
+
+            string firstAuthor = authorPart.Split(new[] { " and " }, StringSplitOptions.None)[0].Trim();
+
+            return (title, firstAuthor);
+
+        }
+
+        static List<string> SortTitles(List<string> bookEntries)
+
+        {
+
+            List<(string Title, string FirstAuthor)> parsedBooks = new List<(string, string)>();
+
+            foreach (string entry in bookEntries)
+
+            {
+
+                var parsed = ParseEntry(entry);
+
+                parsedBooks.Add(parsed);
+
+            }
+
+            parsedBooks.Sort((a, b) =>
+
+            {
+
+                int authorCompare = a.FirstAuthor.CompareTo(b.FirstAuthor);
+
+                if (authorCompare == 0)
+
+                {
+
+                    return a.Title.CompareTo(b.Title);
+
+                }
+
+                return authorCompare;
+
+            });
+
+            List<string> sortedTitles = new List<string>();
+
+            foreach (var item in parsedBooks)
+
+            {
+
+                sortedTitles.Add(item.Title);
+
+            }
+
+            return sortedTitles;
+
+        }
+
+        static void Main()
+
+        {
+
+            List<string> books = new List<string>
+            {
+                "\"The Canterbury Tales\" by Chaucer",
+
+                "\"Algorithms\" by Sedgewick",
+
+                "\"The C Programming Language\" by Kernighan and Ritchie"
+
+            };
+
+            List<string> sortedTitles = SortTitles(books);
+
+            Console.WriteLine("Sorted Titles:");
+
+            foreach (string title in sortedTitles)
+
+            {
+
+                Console.WriteLine(title);
+
+            }
+
+        }
+
+    }
+
+}

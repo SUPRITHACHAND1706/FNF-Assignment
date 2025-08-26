b1cafd35b8a13e7c930b5d08fab5bc2c8ad374a5
SUPRITHACHAND1706 <160388279+SUPRITHACHAND1706@users.noreply.github.com>
2025-08-26 13:00:02 +0530

    Add files via upload

---
+++ Hackathonweb/Test01Search.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Test01Search.aspx.cs" Inherits="SampleWbFormsApp.Hackathon2.Test01Search" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h2>Search word</h2>
+        <p>
+            English word:<asp:TextBox ID="txtSearch" runat="server"></asp:TextBox>
+            <asp:Button ID="btnSearch" runat="server" BackColor="LightBlue" BorderStyle="Inset"  Text="Search" OnClick="btnSearch_Click" />
+            <br />
+            <asp:Label ID ="lblMsg" runat="server" ForeColor="Red" />
+            <asp:Panel ID ="pnlAdd" runat="server" Visible="false" />
+            <p>
+                <table>
+                    <tr>
+                        <td>Word   </td>
+                        <td>Translation </td>
+                        <td>Action </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <asp:Label ID="lblWord" runat="server" /></td>
+                        <td>
+                            <asp:TextBox ID="txtTranslation" runat="server" /></td>
+                        <td>
+                            <asp:Button ID="btnAdd" Text="Add to My Words" runat="server" OnClick="btnAdd_Click" />
+                        </td>
+                    </tr>
+                </table>
+                </asp:Panel>
+                    <br />
+                    <a href="Test01Words.aspx">My Words</a>
+    </form>
+</body>
+</html>
+
---
+++ Hackathonweb/Test01Search.aspx.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SampleWbFormsApp.Hackathon2
+{
+    public partial class Test01Search : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Words.Count == 0)
+                {
+                    Words.Add("Home", "");
+                    Words.Add("fun", "");
+                    Words.Add("Sequel", "");
+                }
+                BindGrid();
+                txtTranslation.Visible = false;
+            }
+        }
+        private Dictionary<string, string> Words
+        {
+            get
+            {
+                if (Session["Words"] == null)
+                    Session["Words"] = new Dictionary<string, string>();
+                return (Dictionary<string, string>)Session["words"];
+            }
+        }
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            string searchWord = txtSearch.Text.Trim();
+            var found = Words.FirstOrDefault(w => w.Key.Equals(searchWord, StringComparison.OrdinalIgnoreCase));
+            if (!string.IsNullOrEmpty(found.Key))
+            {
+                if (string.IsNullOrEmpty(found.Value))
+                {
+                    lblMsg.Text = $"Word '{found.Key}' found. Please add its translation.";
+                    txtTranslation.Visible = true;
+                    txtTranslation.Text = "";
+                }
+                else
+                {
+                    lblMsg.Text = $"Word: {found.Key}, Translation: {found.Value}";
+                    txtTranslation.Visible = false;
+                }
+            }
+            else
+            {
+                Response.Redirect("WordNotFound.aspx?word=" + Server.UrlEncode(searchWord));
+            }
+
+            BindGrid();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            string word = txtSearch.Text.Trim();
+            string translation = txtTranslation.Text.Trim();
+
+            Words[word] = translation; 
+            lblMsg.Text = $"Translation for '{word}' added: {translation}";
+            txtTranslation.Visible = false;
+            BindGrid();
+        }
+        private void BindGrid()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Word");
+            dt.Columns.Add("Translation");
+
+            foreach (var kv in Words)
+            {
+                dt.Rows.Add(kv.Key, kv.Value);
+            }
+            var grid = this.FindControl("gvWords") as GridView;
+            if (grid != null)
+            {
+                grid.DataSource = dt;
+                grid.DataBind();
+            }
+        }
+    }
+}
